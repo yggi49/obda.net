a2fca04ad026efea7322b250648731952c4235c5
Clemens Kaposi <clemens@kaposi.name>
2023-04-16 21:25:07 +0200

    Fix ruff violations for fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
-from fabric import task, Connection
+"""obda.net deployment script."""
+
+from fabric import Connection, task
 from invoke import run as local
 
 
 @task
-def deploy(ctx):
+def deploy(ctx: Connection) -> None:
     """Deploy the blog to the production system.
 
     Do not forget to supply ``--prompt-for-sudo-password`` when running
@@ -11,16 +13,16 @@ def deploy(ctx):
 
        fab -H <host> --prompt-for-sudo-password deploy
     """
-    archive = "/tmp/obda.net.tar.gz"
-    requirements = "/tmp/obda.net.requirements.txt"
-    local("git archive -o {} HEAD".format(archive))
-    local("poetry export -o {}".format(requirements))
+    archive = "/tmp/obda.net.tar.gz"  # noqa: S108
+    requirements = "/tmp/obda.net.requirements.txt"  # noqa: S108
+    local(f"git archive -o {archive} HEAD")
+    local(f"poetry export -o {requirements}")
     for path in (archive, requirements):
         ctx.put(path, path)
-    ctx.sudo("tar -C /srv/obda/blog -xzf {}".format(archive))
-    ctx.sudo("/srv/obda/blog/venv/bin/pip install -r {}".format(requirements))
+    ctx.sudo(f"tar -C /srv/obda/blog -xzf {archive}")
+    ctx.sudo(f"/srv/obda/blog/venv/bin/pip install -r {requirements}")
     ctx.sudo("chown -R obda:obda /srv/obda/blog")
     ctx.sudo("touch /etc/uwsgi.d/obda.ini")
     for path in (archive, requirements):
-        ctx.run("rm {}".format(path))
-        local("rm {}".format(path))
+        ctx.run(f"rm {path}")
+        local(f"rm {path}")
